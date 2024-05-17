877590e8945d6bc013100aa5063c8e8e05405c44
Raulochoa1989 <quimicoraul@gmail.com>
2024-05-17 16:28:57 -0400

    asd

--- js/producto.js
+++ js/producto.js
@@ -1,16 +1,26 @@
 //definiciaon de variables
 const tasa = document.getElementById("tasa_cambio")
-let factor = 36.61
+let factor = 36.63
 const preciosRef = document.getElementsByClassName("ref")
 const preciosBs = document.getElementsByClassName("precio")
 const btnSumar = document.querySelectorAll(".sumar")
 const btnRestar = document.querySelectorAll(".restar")
 const btnAgregarItem = document.querySelectorAll(".agregarcarrito")
 const productoselect = document.getElementById("contlista")
-const btneliminar = document.querySelectorAll("eliminarproducto")
+const btnLimpiarCarrito = document.getElementById("limpiarcarrito")
+const btnConfirmarPedido = document.getElementById("confirmarpedido")
 let productoscarrito = document.getElementById("productoscarrito")
+let montorefcarrito = document.getElementById("totalrefcarrito")
+let montobscarrito = document.getElementById("totalbscarrito")
 let idProductosCarrito =[]
 let carrito = 0
+let pedido = {
+    items :[],
+    totalRef: 0,
+    totalBs: 0
+}
+
+
 const productos =[
     pro1 = {
         imagen: "recursos/Imagenes/Lavaplatos.jpg",
@@ -81,9 +91,31 @@ btnRestar.forEach(sustraccion =>{
 btnAgregarItem.forEach(cargaProducto =>{
     cargaProducto.addEventListener("click", cargarProducto)
 })
-btneliminar.forEach(eliminarproductocarrito =>{
-    eliminarproductocarrito.addEventListener("click,",eliminarProducto)
-})
+btnLimpiarCarrito.addEventListener("click",limpiarCarrito)
+
+btnConfirmarPedido.addEventListener('click', () => {
+    let lista = pedido.items
+    let titulos = document.getElementsByClassName("productopedido")
+    let cantidades = document.getElementsByClassName("cantidadpedido")
+    let montosRef = document.getElementsByClassName("montoref")
+    let montosBs = document.getElementsByClassName("montobs")
+    let totalRef = document.getElementById("totalrefcarrito")
+    let totalBs = document.getElementById("totalbscarrito")
+    for (let i = 0; i < titulos.length; i++) {
+        const titulo = titulos.item(i).innerText
+        const cantidad = cantidades.item(i).innerText.slice(10)
+        const montoRef = montosRef.item(i).innerText.split(":")[1].slice(0,-1)
+        const montoBs = montosBs.item(i).innerText.split(":")[1].slice(0,-2)
+        lista.push({titulo:titulo,cantidad:cantidad,montoRef:montoRef,montoBs:montoBs})
+    }
+    pedido.totalRef = totalRef.innerText
+    pedido.totalBs = totalBs.innerText 
+    let mensaje = formatearMensaje(pedido);
+    let numeroWhatsApp = '584149616631'; // Número de WhatsApp al que se enviará el mensaje
+    let url = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensaje)}`;
+    window.open(url);
+});
+
 
 
 // definicion de funciones
@@ -148,8 +180,10 @@ function cargarProducto(){
         var nuevoMontoBs = document.getElementById(`montobs${idProducto}`)
         var nuevaCantidad = (parseFloat(cantidad) + parseFloat(cantidadActual.innerText.slice(10)))
         cantidadActual.innerHTML = `cantidad: ${nuevaCantidad}`
-        nuevoMontoRef.innerHTML = `${(objSeleccionado.precioRef * nuevaCantidad).toFixed(2)}$`
-        nuevoMontoBs.innerHTML = `${(montoBs*nuevaCantidad).toFixed(2)}Bs`
+        nuevoMontoRef.innerHTML = `monto Ref: ${(objSeleccionado.precioRef * nuevaCantidad).toFixed(2)}$`
+        nuevoMontoBs.innerHTML = `monto Bs: ${(montoBs*nuevaCantidad).toFixed(2)}Bs`
+        actualizarTotalesCarrito()
+        alert("Producto Modificado en carrito")
     } else {
         idProductosCarrito.push(idObjSeleccionado)
         var cantidad = this.parentNode.getElementsByClassName('cantidad')[0].value
@@ -174,19 +208,76 @@ function cargarProducto(){
                         <p class="productopedido">${objSeleccionado.descripcion}</p>
                         <p class="cantidadpedido" id="cantidad${idProducto}">cantidad: ${cantidad}</p>
                         <p class="montoref" id="montoref${idProducto}">monto Ref: ${(objSeleccionado.precioRef * cantidad).toFixed(2)}$</p>
-                        <p class="montobs" id="montobs${idProducto}"> monto Bs: ${(montoBs*cantidad).toFixed(2)}Bs</p>
+                        <p class="montobs" id="montobs${idProducto}">monto Bs: ${(montoBs*cantidad).toFixed(2)}Bs</p>
                     </div>
                 </div>`
             productoselect.append(div)
             carrito++
             productoscarrito.innerText = carrito
+        alert("Producto agregado al carrito de compras")
     }
+    actualizarTotalesCarrito()
 }
-function eliminarProducto(){
-    const idProducto = this.parentNode.getElementsByClassName('idproducto')[0].id;
-    alert (idProducto)
+function limpiarCarrito(){
+    productoselect.innerHTML = ""
+    carrito = 0
+    productoscarrito.innerText = 0
+    idProductosCarrito.length = 0
+    montorefcarrito.innerHTML = 0
+    montobscarrito.innerHTML = 0
+    pedido.items= []
+    pedido.totalBs = 0
+    pedido.totalRef =0
+    console.log(pedido)
 }
+function actualizarTotalesCarrito(){
+    var total = 0
+    var total2 = 0
+    var divRef = document.querySelectorAll(".montoref")
+    var divBs = document.querySelectorAll(".montobs")
+    for (let i = 0; i < divRef.length; i++) {
+        let precio = parseFloat(divRef.item(i).innerText.split(":")[1].slice(0,-1))
+        total += precio
+    }
+    montorefcarrito.textContent = `${total.toFixed(2).toString()}$`
+    
+    for (let i = 0; i < divBs.length; i++) {
+        let precio2 = parseFloat(divBs.item(i).innerText.split(":")[1].slice(0,-1))
+        total2 += precio2
+    }
+    montobscarrito.textContent = `${total2.toFixed(2).toString()}Bs`
+} 
+/*function AgregarDetallesPedido(){ ----> se intento agregar esta funcion al boton confirmar pedido pero no resulto preguntar al profe porque puede ser!!
+    let lista = pedido.items
+    let titulos = document.getElementsByClassName("productopedido")
+    let cantidades = document.getElementsByClassName("cantidadpedido")
+    let montosRef = document.getElementsByClassName("montoref")
+    let montosBs = document.getElementsByClassName("montobs")
+    let totalRef = document.getElementById("totalrefcarrito")
+    let totalBs = document.getElementById("totalbscarrito")
+    for (let i = 0; i < titulos.length; i++) {
+        const titulo = titulos.item(i).innerText
+        const cantidad = cantidades.item(i).innerText.slice(10)
+        const montoRef = montosRef.item(i).innerText.split(":")[1].slice(0,-1)
+        const montoBs = montosBs.item(i).innerText.split(":")[1].slice(0,-2)
+        lista.push({titulo:titulo,cantidad:cantidad,montoRef:montoRef,montoBs:montoBs})
+    }
+    pedido.totalRef = totalRef.innerText
+    pedido.totalBs = totalBs.innerText 
+}*/
 
+function formatearMensaje(pedido) {
+    let mensaje = 'Se ha generado un pedido con los siguientes productos:\n';
+    mensaje+=`\n`
+    pedido.items.forEach(item => {
+        mensaje += `${item.cantidad}L ${item.titulo} ${item.montoBs}Bs (${item.montoRef}$)\n`;
+    });
+    mensaje+=`\n`
+    mensaje += `Total: ${pedido.totalBs} (${pedido.totalRef})`;
+    mensaje += `\n`
+    mensaje += `¿Porfavor podrían confirmar existencia de los mismos?`
+    return mensaje;
+}
 
 
 
